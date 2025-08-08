df3b3a7518dd4958f0af4d4982648495c5b89afc
Kilogranm <116014858+Kilogranm@users.noreply.github.com>
2025-08-08 08:15:57 +0300

    Update models.py

--- app/database/models.py
+++ app/database/models.py
@@ -3,6 +3,9 @@ from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from sqlalchemy.ext.asyncio import AsyncAttrs, async_sessionmaker, create_async_engine
 import os
 
+load_dotenv()
+
+
 DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_async_engine(DATABASE_URL, echo=True)
